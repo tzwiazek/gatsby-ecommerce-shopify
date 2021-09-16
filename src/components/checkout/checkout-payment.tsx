cd85e0f3bac13776283c6a6959b7917c640e07e2
tomasz.zwiazek <tomasz.zwiazek0@gmail.com>
2021-09-16 12:53:23 +0200

    migrate

--- src/components/checkout/checkout-payment.tsx
+++ src/components/checkout/checkout-payment.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
-import { StripeProvider, Elements } from 'react-stripe-elements';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import { CartContext, CartContextType } from '../cart/cart-provider';
 import { CartUIContext } from 'src/components/cart/cartUI';
 import Checkout from './checkout';
@@ -10,6 +11,9 @@ const Payment = () => {
   const [complete, updateComplete] = useState<boolean>(false);
   const [cart, , emptyCart] = useContext<CartContextType>(CartContext);
   const [, updateCartUI] = useContext<string>(CartUIContext);
+  const stripePromise: any = loadStripe(
+    'pk_test_51J4opuDC7y7WZSIzduurTII5oItC72Ma5VOXcBzzPMt75YAL60nu6zCFlOD5a8uLZ2SOx6pF5ZervT8HwGG6o1FF00DQyH2MhE'
+  );
 
   return (
     <CardStyled>
@@ -34,17 +38,15 @@ const Payment = () => {
         <span className="cc-number"> 4242 4242 4242 4242</span>, and enter any 5 digits for the zip
         code
       </small>
-      <StripeProvider apiKey="pk_test_51J4opuDC7y7WZSIzduurTII5oItC72Ma5VOXcBzzPMt75YAL60nu6zCFlOD5a8uLZ2SOx6pF5ZervT8HwGG6o1FF00DQyH2MhE">
-        <Elements>
-          <Checkout
-            updateComplete={updateComplete}
-            email={email}
-            complete={complete}
-            cart={cart}
-            emptyCart={emptyCart}
-            updateCartUI={updateCartUI}></Checkout>
-        </Elements>
-      </StripeProvider>
+      <Elements stripe={stripePromise}>
+        <Checkout
+          updateComplete={updateComplete}
+          email={email}
+          complete={complete}
+          cart={cart}
+          emptyCart={emptyCart}
+          updateCartUI={updateCartUI}></Checkout>
+      </Elements>
     </CardStyled>
   );
 };
@@ -62,8 +64,7 @@ const Success = () => {
 };
 
 const Card = () => {
-  const [cartUIStatus]: [string, React.Dispatch<React.SetStateAction<string>>] =
-    useContext(CartUIContext);
+  const [cartUIStatus] = useContext<string>(CartUIContext);
   return (
     <>
       {cartUIStatus === 'checkout' ? <Payment /> : null}
